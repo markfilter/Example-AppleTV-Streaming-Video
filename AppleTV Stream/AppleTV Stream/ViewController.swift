41179ce815ab213e4697354218ca3a32251a6dc6
Mark Filter <markfilter@outlook.com>
2018-02-19 14:20:53 -0500

    Adds project files

---
+++ AppleTV Stream/AppleTV Stream/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppleTV Stream
+//
+//  Created by Mark Filter on 2/19/18.
+//  Copyright © 2018 Mark Filter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
