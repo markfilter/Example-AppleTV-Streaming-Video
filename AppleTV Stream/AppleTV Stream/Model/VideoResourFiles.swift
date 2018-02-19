cbb7d1f6f0d74accdce792bcdff4999db6bec739
Mark Filter <markfilter@outlook.com>
2018-02-19 14:35:57 -0500

    Adds remote endpoints

---
+++ AppleTV Stream/AppleTV Stream/Model/VideoResourFiles.swift
@@ -0,0 +1,57 @@
+//
+//  VideoResourFiles.swift
+//
+//  Created by Mark Filter on 2/19/18.
+//  These endpoints are the same endpoints that Android Studio provides in the stubbed Android TV application.
+
+import Foundation
+
+public class RemoteVideoResources {
+    
+    public static let testDomain = "https://commondatastorage.googleapis.com/"
+    
+    public static let titles : [String] = [
+        "Zeitgeist 2010_ Year in Review",
+        "Google Demo Slam_ 20ft Search",
+        "Introducing Gmail Blue",
+        "Introducing Google Fiber to the Pole",
+        "Introducing Google Nose"
+    ]
+    
+    public static let description : String = "Fusce id nisi turpis. Praesent viverra bibendum semper. "
+        + "Donec tristique, orci sed semper lacinia, quam erat rhoncus massa, non congue tellus est "
+        + "quis tellus. Sed mollis orci venenatis quam scelerisque accumsan. Curabitur a massa sit "
+        + "amet mi accumsan mollis sed et magna. Vivamus sed aliquam risus. Nulla eget dolor in elit "
+        + "facilisis mattis. Ut aliquet luctus lacus. Phasellus nec commodo erat. Praesent tempus id "
+        + "lectus ac scelerisque. Maecenas pretium cursus lectus id volutpat."
+    
+    public static let studio : [String] = [
+        "Studio Zero", "Studio One", "Studio Two", "Studio Three", "Studio Four"
+    ]
+    
+    public static let videoUrl : [String] = [
+        "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/Zeitgeist/Zeitgeist%202010_%20Year%20in%20Review.mp4",
+        "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/Demo%20Slam/Google%20Demo%20Slam_%2020ft%20Search.mp4",
+        "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Gmail%20Blue.mp4",
+        "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Fiber%20to%20the%20Pole.mp4",
+        "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Nose.mp4"
+    ]
+    
+    let bgImageUrl : [String] = [
+        "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/Zeitgeist/Zeitgeist%202010_%20Year%20in%20Review/bg.jpg",
+        "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/Demo%20Slam/Google%20Demo%20Slam_%2020ft%20Search/bg.jpg",
+        "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Gmail%20Blue/bg.jpg",
+        "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Fiber%20to%20the%20Pole/bg.jpg",
+        "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Nose/bg.jpg",
+        ]
+    
+    public static let cardImageUrl : [String] = [
+        "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/Zeitgeist/Zeitgeist%202010_%20Year%20in%20Review/card.jpg",
+        "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/Demo%20Slam/Google%20Demo%20Slam_%2020ft%20Search/card.jpg",
+        "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Gmail%20Blue/card.jpg",
+        "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Fiber%20to%20the%20Pole/card.jpg",
+        "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Nose/card.jpg"
+    ]
+    
+}
+
